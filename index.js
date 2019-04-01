bb82c5fb6eefbc856a742707b399e8ece3ab19d6
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-01 10:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var name = "Joe"
 var height = "74"
-
+if (name === "Susan") {
+    console.log("The name is correct")
+}else if (name === "Joe")
+    console.log("This is the right name")
 
 // Don't worry about this
 module.exports = { name, height
